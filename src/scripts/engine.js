2848d283ee57d719d1159c209263b59027c23def
Luiz Eduardo <luizeduardo137b@gmail.com>
2025-06-17 22:52:43 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,64 @@
+ window.onload = function() {
+ const bgMusic = document.getElementById("bgMusic");
+
+ bgMusic.volume = 0.1;
+  
+bgMusic.play().catch(e => {
+console.log("Autoplay bloqueado. A música só tocará após interação do usuário.");
+ });
+  
+ document.body.addEventListener("click", () => {
+bgMusic.play();
+});
+};
+
+
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeleft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        timerId: null, 
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+    },
+};
+
+function randomSquare(){
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function moveEnemy(){
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", ()=>{
+       if(square.id === state.values.hitPosition){
+        state.values.result++
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+       }   
+        });
+    });
+}
+
+function initialize() {
+    moveEnemy();
+    randomSquare();
+    addListenerHitBox();
+}
+
+initialize();
